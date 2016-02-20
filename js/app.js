e23bcc08e495e98b97fae2baefd37657c3870c68
Hari <infotech.hari@gmail.com>
2016-02-20 19:59:25 +0530

    Having header title and menu issue

--- js/app.js
+++ js/app.js
@@ -10,6 +10,12 @@ app.factory('utilityFun', function() {
         };
     });
 
+app.run(['$rootScope', function($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);
+
 
  
 //Define Routing for app
@@ -19,18 +25,22 @@ app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/addActivity', {
+        title: 'Add Activity',
         templateUrl: 'templates/add_activity.html',
         controller: 'AddOrderController'
       }).
       when('/metrics', {
+        title: 'Metrics',
         templateUrl: 'templates/metrics.html',
         controller: 'ShowMetrics'
       }).
       when('/feed', {
+        title: 'Feeds',
         templateUrl: 'templates/timeline.html',
         controller: 'FeedController'
       }).
       when('/badges', {
+        title: 'Badges',
         templateUrl: 'templates/badges.html',
         controller: 'BadgesController'
       }).
@@ -119,154 +129,18 @@ app.controller('ShowMetrics', function($scope,utilityFun) {
             }]
         }
     });
-
-    var data = Highcharts.geojson(Highcharts.maps['countries/us/us-all']),
-        // Some responsiveness
-        small = $('#container1').width() < 400;
-
-    // Set drilldown pointers
-    $.each(data, function (i) {
-        this.drilldown = this.properties['hc-key'];
-        this.value = i; // Non-random bogus data
-    });
-
-    // Instanciate the map
-    $('#container1').highcharts('Map', {
-        chart : {
-            events: {
-                drilldown: function (e) {
-
-                    if (!e.seriesOptions) {
-                        var chart = this,
-                            mapKey = 'countries/us/' + e.point.drilldown + '-all',
-                            // Handle error, the timeout is cleared on success
-                            fail = setTimeout(function () {
-                                if (!Highcharts.maps[mapKey]) {
-                                    chart.showLoading('<i class="icon-frown"></i> Failed loading ' + e.point.name);
-
-                                    fail = setTimeout(function () {
-                                        chart.hideLoading();
-                                    }, 1000);
-                                }
-                            }, 3000);
-
-                        // Show the spinner
-                        chart.showLoading('<i class="icon-spinner icon-spin icon-3x"></i>'); // Font Awesome spinner
-
-                        // Load the drilldown map
-                        $.getScript('https://code.highcharts.com/mapdata/' + mapKey + '.js', function () {
-
-                            data = Highcharts.geojson(Highcharts.maps[mapKey]);
-
-                            // Set a non-random bogus value
-                            $.each(data, function (i) {
-                                this.value = i;
-                            });
-
-                            // Hide loading and add series
-                            chart.hideLoading();
-                            clearTimeout(fail);
-                            chart.addSeriesAsDrilldown(e.point, {
-                                name: e.point.name,
-                                data: data,
-                                dataLabels: {
-                                    enabled: true,
-                                    format: '{point.name}'
-                                }
-                            });
-                        });
-                    }
-
-
-                    this.setTitle(null, { text: e.point.name });
-                },
-                drillup: function () {
-                    this.setTitle(null, { text: 'USA' });
-                }
-            }
-        },
-
-        title : {
-            text : 'Highcharts Map Drilldown'
-        },
-
-        subtitle: {
-            text: 'USA',
-            floating: true,
-            align: 'right',
-            y: 50,
-            style: {
-                fontSize: '16px'
-            }
-        },
-
-        legend: small ? {} : {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
-        },
-
-        colorAxis: {
-            min: 0,
-            minColor: '#E6E7E8',
-            maxColor: '#005645'
-        },
-
-        mapNavigation: {
-            enabled: true,
-            buttonOptions: {
-                verticalAlign: 'bottom'
-            }
-        },
-
-        plotOptions: {
-            map: {
-                states: {
-                    hover: {
-                        color: '#EEDD66'
-                    }
-                }
-            }
-        },
-
-        series : [{
-            data : data,
-            name: 'USA',
-            dataLabels: {
-                enabled: true,
-                format: '{point.properties.postal-code}'
-            }
-        }],
-
-        drilldown: {
-            //series: drilldownSeries,
-            activeDataLabelStyle: {
-                color: '#FFFFFF',
-                textDecoration: 'none',
-                textShadow: '0 0 3px #000000'
-            },
-            drillUpButton: {
-                relativeTo: 'spacingBox',
-                position: {
-                    x: 0,
-                    y: 60
-                }
-            }
-        }
-    });
  
 });
  
 app.controller('FeedController', function($scope,utilityFun) {
  
-    $(".cd-stretchy-nav a").removeClass("active");
     utilityFun.makeActive("feed");
  
 });
 
 app.controller('BadgesController', function($scope) {
  
-    $("#badges").addClass ("active");
+    utilityFun.makeActive("badges");
  
 });
 
