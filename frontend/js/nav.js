edeefb8e533317a72ad7723633bd964c86d68dff
IzumiSy <beetle-noise@gmx.com>
2016-02-21 17:36:06 -0800

    Fix z-index of nav

--- frontend/js/nav.js
+++ frontend/js/nav.js
@@ -1,19 +1,15 @@
 jQuery(document).ready(function(){
-	if( $('.cd-stretchy-nav').length > 0 ) {
-		var stretchyNavs = $('.cd-stretchy-nav');
-		
-		stretchyNavs.each(function(){
-			var stretchyNav = $(this),
-				stretchyNavTrigger = stretchyNav.find('.cd-nav-trigger');
-			
-			stretchyNavTrigger.on('click', function(event){
-				event.preventDefault();
-				stretchyNav.toggleClass('nav-is-visible');
-			});
-		});
+  var stretchyNav = $('.cd-stretchy-nav');
 
-		$(document).on('click', function(event){
-			( !$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span') ) && stretchyNavs.removeClass('nav-is-visible');
-		});
-	}
+  stretchyNav.on('click', function(event) {
+    stretchyNav.css('height', stretchyNav.hasClass('nav-is-visible') ? 'initial' : '60px');
+
+    // if "li" element is clicked.
+    if (listElement = $(event.target).parents("li")[0]) {
+      stretchyNav.css('height', '60px');
+    }
+  });
+
+  // set default height as 60px
+  stretchyNav.css('height', '60px');
 });
